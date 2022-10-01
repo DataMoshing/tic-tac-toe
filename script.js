ea358cc0dabf91facd849effe235dab4a6471a9d
datamoshing <imkiloss@gmail.com>
2022-10-01 17:09:29 -0600

    Created a module that manages game

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-// Create gameboard as an array inside Gameboard object (module)
-const Gameboard = {
-    gameboard: [],
-}
+// Create gameboard as an array inside Gameboard object (module/IIFE)
+const gameBoardModule = (() => {
 
+    // Array with nine indexes 
+    let gameboard = new Array(9).fill("")
+
+    // Update array index with players value
+    const setCell = (index, value) => gameboard[index] = value
+
+    // Reset the game board
+    const resetBoard = () => {
+        gameboard = ["", "", "", "", "", "", "", "", "",]
+    }
+    // Get a copy of the original array
+    const getBoard = () => {
+        const boardClone = [...gameboard]
+        console.log(boardClone)
+        return boardClone
+    }
+    return {
+        setCell,
+        resetBoard,
+        getBoard
+    }
+})();
+
+gameBoardModule.resetBoard()
+
+
+// 2 player game which the players are also objects (factory functions)
 const createPlayer = (player1, player2) => {
     return {
-        player1: player1,
-        player2: player2
+        player1,
+        player2,
+        greet() {
+            console.log("Greetings " + player1 + " and " + player2)
+        }
     }
 }
-// 2 player game which the players are also objects (factory functions)
+
+const test2 = createPlayer("A", "B")
+
+test2.greet();
+
 // Use DOM Manipulation to render HTML (X's and O's)
 // Create factory functions to let players mark the gameboard
 // Check if spots are already taken
